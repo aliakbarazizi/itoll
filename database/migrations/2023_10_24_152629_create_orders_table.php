a96f521cfe87ecf86f32b05dc5b6c49e5dd30cad
Ali Akbar Azizi <aliakbar.azizi20@gmail.com>
2023-10-24 19:06:07 +0330

    feat: add migration

---
+++ database/migrations/2023_10_24_152629_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('from_customer_id');
+            $table->unsignedBigInteger('to_customer_id');
+            $table->string('status');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('from_customer_id')->references('id')->on('customers');
+            $table->foreign('to_customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
