a366ef82549f227e8a5e8c05490657dbd6891942
Ali Akbar Azizi <aliakbar.azizi20@gmail.com>
2023-10-25 02:59:02 +0330

    fix: tests

--- tests/Feature/OrderLocationTest.php
+++ tests/Feature/OrderLocationTest.php
@@ -2,12 +2,47 @@
 
 namespace Tests\Feature;
 
+use App\Events\OrderLocationCreated;
+use App\Models\Enums\OrderStatus;
+use App\Models\Order;
+use App\Models\User;
+use Auth;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class OrderLocationTest extends TestCase
 {
     public function test_update_live_location(): void
     {
+        Event::fake();
 
+        Sanctum::actingAs(User::factory()->driver()->create());
+
+        $order = Order::factory()->for(Auth::user(), 'driver')->create([
+            'status' => OrderStatus::IN_PROGRESS
+        ]);
+
+        for ($i = 0; $i < 10; $i++) {
+            $location = [
+                'latitude' => fake()->latitude,
+                'longitude' => fake()->longitude
+            ];
+
+            $this
+                ->postJson("/api/orders/$order->id/locations", $location)
+                ->assertStatus(201)
+                ->assertJson(fn(AssertableJson $json) => $json
+                    ->whereType('id', 'integer')
+                    ->where('order_id', $order->id)
+                    ->where('latitude', $location['latitude'])
+                    ->where('longitude', $location['longitude'])
+                );
+        }
+
+        $this->assertEquals(10, $order->orderLocations->count());
+
+        Event::assertDispatched(OrderLocationCreated::class, 10);
     }
 }
--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Events\OrderCreated;
+use App\Events\OrderStatusUpdated;
 use App\Models\Customer;
 use App\Models\Enums\OrderStatus;
 use App\Models\Order;
 use App\Models\User;
 use Auth;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -27,6 +30,7 @@ class OrderTest extends TestCase
      */
     public function test_create_order(): void
     {
+        Event::fake();
         Sanctum::actingAs(User::factory()->create());
 
         /** @var Customer $fromCustomer */
@@ -60,10 +64,13 @@ class OrderTest extends TestCase
                 ->where('to_customer', $request['to'])
                 ->where('status', OrderStatus::REGISTERED->value)
             );
+
+        Event::assertDispatched(OrderCreated::class, 1);
     }
 
     public function test_cancel_order(): void
     {
+        Event::fake();
         Sanctum::actingAs(User::factory()->create());
 
         $order = Order::factory()->for(Auth::user())->create([
@@ -78,10 +85,12 @@ class OrderTest extends TestCase
             );
 
         $this->assertEquals(OrderStatus::CANCELLED, $order->fresh()->status);
+        Event::assertDispatched(OrderStatusUpdated::class, 1);
     }
 
     public function test_prevent_cancel_active_order(): void
     {
+        Event::fake();
         Sanctum::actingAs(User::factory()->create());
 
         $order = Order::factory()->for(Auth::user())->create([
@@ -95,6 +104,7 @@ class OrderTest extends TestCase
 
     public function test_accept_order(): void
     {
+        Event::fake();
         Sanctum::actingAs(User::factory()->driver()->create());
 
         $order = Order::factory()->for(Auth::user(), 'driver')->create([
@@ -109,10 +119,12 @@ class OrderTest extends TestCase
             );
 
         $this->assertEquals(OrderStatus::IN_PROGRESS, $order->fresh()->status);
+        Event::assertDispatched(OrderStatusUpdated::class, 1);
     }
 
     public function test_complete_order(): void
     {
+        Event::fake();
         Sanctum::actingAs(User::factory()->driver()->create());
 
         $order = Order::factory()->for(Auth::user(), 'driver')->create([
@@ -127,20 +139,21 @@ class OrderTest extends TestCase
             );
 
         $this->assertEquals(OrderStatus::COMPLETED, $order->fresh()->status);
+        Event::assertDispatched(OrderStatusUpdated::class, 1);
     }
 
     public function test_get_list_of_orders(): void
     {
-
+        $this->markTestIncomplete();
     }
 
     public function test_get_list_of_pending_orders(): void
     {
-
+        $this->markTestIncomplete();
     }
 
     public function test_register_order_race_condition(): void
     {
-
+        $this->markTestIncomplete();
     }
 }
